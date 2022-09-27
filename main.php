9cd7f7c5316582a3e81d4392c92eacd03e132f13
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-09-27 09:52:37 -0400

    Update main.php

--- main.php
+++ main.php
@@ -1,7 +1,47 @@
 <?php
   //include('repositories/raidertime-22/calc.php');
-  require'repositories/raidertime-22/frontEnd/Slide: 1 Choose User/Atholton Logo In Corner';
+  //require'repositories/raidertime-22/frontEnd/Slide: 1 Choose User/Atholton Logo In Corner';
 ?>
+<DOCTYPE html>
+    <html>
+
+<!-- IGNORE THIS CODE, YOU CAN DELETE IT IF YOU WANT -->
+
+    <head>
+        <title>Home Page
+        </title>
+    </head>
+    <rect width=“300”, height=“100”, x=“20”, y=“20”, fill=“green” />
+    <p style="background-color: green;">Welcome Raiders
+        <br>
+        I am a:
+
+        <button type="button">Student</button>
+        <button type="button">Teacher</button>
+        <button type="button">Administrator</button>
+    </p>
+      <style>
+#content {
+        position: relative;
+    }
+    #content img {
+        position: absolute;
+        top: -60px;
+        right: 0px;
+      </style>
+      <div id="content">
+        <a href="https://ahs.hcpss.org/" target="_blank">
+          <img     src="https://raw.githubusercontent.com/atholtonawol/raidertime-22/main/assets/image/atholtonA.png" alt="Atholton Logo" style="width:50px;height:60px;">
+        </a>
+  </div>
+    </html>
+
+
+
+
+
+
+
 <!--
 <html>
 <head>

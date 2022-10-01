ff95073aad8432cd9e76f2165b11644e0d94f216
Kamran Damavandi <40551764+atholtonawol@users.noreply.github.com>
2022-10-01 19:07:12 +0000

    j

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <html>
   <header>
     <title>Atholton Raider Time main.php</title>
-    <base href="/home4/ahsraid1/public_html/repositories/raidertime-22/" target="_blank">
+    <base href="repositories/raidertime-22/" target="_blank">
   </header>
   <body>
     <?php

9592b708bae98d6b2ce29270654c787aa41810c8
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-03 09:43:19 -0400

    Update VOGUE2.php

--- VOGUE2.php
+++ VOGUE2.php
@@ -1,34 +1,33 @@
-<h1>Hello</h1>
-<?php
+<?php>
  require '/home4/ahsraid1/public_html/database/connect.php';
  
  
  
  
  if (!$conn) {
-   die("Connection failed: " . mysqli_connect_error());
+  die("Connection failed: " . mysqli_connect_error());
  }
  echo "Connected successfully";  
  
  
+header('Location: https://www.google.com/');
  
  
- 
-   $sql = 'SELECT firstName FROM emails';
-   $result = mysqli_query($conn, $sql);
-   $var = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  $sql = 'SELECT firstName FROM emails';
+  $result = mysqli_query($conn, $sql);
+  $var = mysqli_fetch_all($result, MYSQLI_ASSOC);
 //   print_r($var);
    
 //   print_r($sql);
 //   print_r($result);
 //   print_r($var);
 echo $var[0]['firstName'];
-   foreach($var as $name)
-   {
+  foreach($var as $name)
+  {
     echo $name['firstName'];
     break;
        
-   }
+  }
    
 //  foreach($var as $name){
 //      echo $name['First_Name'];
@@ -39,4 +38,50 @@ echo $var[0]['firstName'];
 // $result = mysqli_query($conn, $names);
 // $row = mysqli_fetch_assoc($result);
 // echo $row;
-?>
+// ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Home Page
+    </title>
+    <link href='https://fonts.googleapis.com/css?family=Graduate' rel='stylesheet'>
+    <link href='Slide_2.css' rel='stylesheet'>
+<style>
+body {
+    font-family: 'Graduate';font-size: 22px;
+    background-image: url('https://github.com/atholtonawol/raidertime-22/blob/main/assets/image/bw%20atholton%20building%20img.jpg?raw=true');
+    background-size: cover;
+}
+</style>
+  </head> 
+    <body>
+      <img src="https://github.com/atholtonawol/raidertime-22/blob/main/assets/image/atholtonA.png?raw=true", align = "right", width="120">
+  <h1>Atholton High School</h1>
+  <h2>Raider Time Signup</h2>
+  
+
+    <div class="welcomebox">
+        <p>Welcome Raiders
+         </p>
+      
+   <form class="login" method = "POST">
+  <input type="text" id="username" name="username" placeholder="username" required><br>
+
+     <input type="password"id="password" name="lname" placeholder="password" required>
+     <input type="submit" id="login"value="Login"/>
+     </form>
+ 
+      <p id="forgotpass">Forgot password?</p>
+      </div>
+      <div class="createaccount">
+        Don't have an account? <u>Sign up</u>
+      </div>
+      
+      
+      <?php echo $_POST['username'];?>
+      
+
+  </body>
+      <?php header('Location: '. 'https://stackoverflow.com/questions/4871942/how-to-redirect-to-another-page-using-php'); ?>
+</html>

34a299c5aa5528afb031ed986ba76e8a7a0fba18
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-06 09:45:38 -0400

    Update link_test.php

--- link_test.php
+++ link_test.php
@@ -1,17 +1,16 @@
-<?php
-require '/home4/ahsraid1/public_html/database/connect.php';
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
- }
- echo "Connected successfully";  
+<?php 
 
 if(isset($_POST[‘submit’])){
+	require '/home4/ahsraid1/public_html/database/connect.php';
+		if (!$conn) {
+  		die("Connection failed: " . mysqli_connect_error());
+ 		}
+ 		echo "Connected successfully"; 
 	$email = $_POST[‘email’];
 	$pass = $_POST[‘password’];
+	echo $pass;
+	echo $email;
 }
-
-echo $pass;
-echo $email;
 // $sql = “UPDATE emails SET testing = $pass WHERE schoolUser = $email”;
 // //$sql = "INSERT INTO emails (email, testing) values ($email, $pass);
 // $update = mysqli_query($conn, $sql);

ab0ae27e9efb60455c835f62560371c79b1bd849
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-07 09:32:29 -0400

    Update link_test.php

--- link_test.php
+++ link_test.php
@@ -4,6 +4,8 @@ require '/home4/ahsraid1/public_html/database/connect.php';
 	if (!$conn) {
   		die("Connection failed: " . mysqli_connect_error());
  	} 
+$yes = $_POST['submit'];\
+echo $yes;
 isset($_POST[‘submit’]){
 	$email = $_POST[‘username’];
 	$pass = $_POST[‘lname’];

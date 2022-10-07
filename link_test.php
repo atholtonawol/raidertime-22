1c078b31d822b5d7296c8b8df3d2a9b7b58f4698
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-07 09:21:10 -0400

    Update link_test.php

--- link_test.php
+++ link_test.php
@@ -1,5 +1,5 @@
 <?php 
-require '/link_test.html';
+require 'link_test.html';
 require '/home4/ahsraid1/public_html/database/connect.php';
 	if (!$conn) {
   		die("Connection failed: " . mysqli_connect_error());

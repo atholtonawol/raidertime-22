a247a89b6a35314e5a527dc5bf7e424d5bacb999
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-07 09:20:23 -0400

    Update VOGUE2.php

--- VOGUE2.php
+++ VOGUE2.php
@@ -11,7 +11,7 @@
  if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
  }
- else{echo "Connected successfully"};  
+ else{echo "Connected successfully";}  
  
 //  echo "<script type='text/javascript'>window.top.location='https://www.google.com//';
 //  </script>"; exit;

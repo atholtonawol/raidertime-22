8c0d43d2a9a46ac2e729e9d0f66737323dcc437d
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-10 09:41:38 -0400

    Update slide_6_get.php

--- slide_6_get.php
+++ slide_6_get.php
@@ -7,4 +7,22 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 } 
 
+//$userName = $_POST["username"];
+$userName = "ashahe8615";
+
+
+ if(isset($_POST['username']))
+ {
+  $sql = 'IF EXISTS (SELECT firstName FROM emails WHERE schoolUser={$userName})';
+  $result = mysqli_query($conn, $sql);
+  echo "oxenberg";
+  if($result)
+  {
+  $sql = 'SELECT firstName FROM emails WHERE schoolUser = {$userName}';
+  $result = mysqli_query($conn, $sql);
+  $var = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  echo $result[0]['firstName'];
+  }
+ } else {echo "did not work";}
+
 ?>

fe0e16b63422ee652a4ac24dbdeb65e6c4905163
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-10 09:55:11 -0400

    Update slide_6_get.php

--- slide_6_get.php
+++ slide_6_get.php
@@ -1,31 +1,4 @@
 <?php
 echo("SLIDE 6 GET");
 
-require 'link_test.html';
-require '/home4/ahsraid1/public_html/database/connect.php';
-/*
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-} 
-
-//$userName = $_POST["username"];
-$userName = "ashahe8615";
-
-
- if(isset($_POST['username']))
- {
-  $sql = 'IF EXISTS (SELECT firstName FROM emails WHERE schoolUser={$userName})';
-  $result = mysqli_query($conn, $sql);
-  echo "oxenberg";
-  if($result)
-  {
-    $sql = 'SELECT firstName FROM emails WHERE schoolUser = {$userName}';
-    $result = mysqli_query($conn, $sql);
-    $var = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    echo $result[0]['firstName'];
-  }
- } else {echo "did not work";}
-
-$first_name = "yesysgf";
-*/
 ?>

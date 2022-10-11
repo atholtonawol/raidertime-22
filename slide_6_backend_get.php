9f80e298d1822278ffb0dde07082517ac170e058
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-11 09:26:41 -0400

    Update slide_6_backend_get.php

--- slide_6_backend_get.php
+++ slide_6_backend_get.php
@@ -1,7 +1,7 @@
 <?php
 echo("SLIDE 6 GET<br>");
 
-require '/home4/ahsraid1/public_html/database/connect.php';
+//require '/home4/ahsraid1/public_html/database/connect.php';
 
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());

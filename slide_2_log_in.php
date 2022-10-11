ccd3bed8b92eb845756e4e6f7a2b874400dab95a
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-11 08:02:38 -0400

    Update slide_2_log_in.php

--- slide_2_log_in.php
+++ slide_2_log_in.php
@@ -1,44 +1,63 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
-    <title>Home Page
-    </title>
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="slide_2.css">
     <link href='https://fonts.googleapis.com/css?family=Graduate' rel='stylesheet'>
-    <link href='slide_2.css' rel='stylesheet'>
+    
 <style>
-body {
+  body {
     font-family: 'Graduate';font-size: 22px;
-    background-image: url('https://github.com/atholtonawol/raidertime-22/blob/main/assets/image/bw%20atholton%20building%20img.jpg?raw=true');
+    background-image: url('https://github.com/atholtonawol/raidertime-22/blob/main/assets/image/color%20atholton%20building.jpg?raw=true');
     background-size: cover;
-}
+    background-repeat: no-repeat;
+    background-color: gainsboro;
+    
+  }
+
 </style>
   </head> 
     <body>
-      <?php
-        require('menu_bar.html');
-      ?>
-      <img src="https://github.com/atholtonawol/raidertime-22/blob/main/assets/image/atholtonA.png?raw=true", align = "right", width="120">
-  <h1>Atholton High School</h1>
-  <h2>Raider Time Signup</h2>
-  
-
-    <div class="welcomebox">
-        <p>Welcome Raiders
-         </p>
+    
+      <div class=" px-4 pt-3 container-fluid">
+      <div class="row">
+        <div class="col-10">
+          <h1 class="display-2"><b>Atholton High School</b></h1>
+            <h2 class="display-4"><b>Raider Time Signup</b></h2>
+        </div>
+        <div class="col-2 text-end">
+          <img class = "img-fluid" src="https://github.com/atholtonawol/raidertime-22/blob/main/assets/image/NewAtholtonA.png?raw=true">
+        </div>
+      </div>
+      </div>
+    <br/>
       
-   <form class="login" method = "POST">
-  <input type="text" id="username" name="username" placeholder="username" required><br>
-
-     <input type="password"id="password" name="lname" placeholder="password" required>
-     <input type="submit" id="login"value="Login"/>
-     </form>
- 
-      <p id="forgotpass"><a href="slide_3_forgot_password.php">Forgot password?</a></p>
+    <div class="container-fluid">
+      <div class="row justify-content-center text-center">
+      <div class="welcomebox col-md-4 col-9 mt-5 justify-content-center text-center">    
+        <p>Welcome Raiders
+          <br>I am a:</p>
+  
+          <form class="login" method = "POST">
+            <input type="text" id="username" name="username" placeholder="username" required><br>
+          
+               <input type="password"id="password" name="lname" placeholder="password" required>
+               <br>
+               <input type="submit" id="login"value="LOGIN"/>
+               </form>
+           
+                <p id="forgotpass"><u>Forgot password?</u></p>
+                <div class="createaccount">
+                  Don't have an account? <u>Sign up</u>
+                </div>  
+              
+              </div>
+                
+        
       </div>
-      <div class="createaccount">
-        Don't have an account? <u>Sign up</u>
       </div>
-      <a href="slide_6_home.php">temp login button</a>
+    </div>
+       
   </body>
-
 </html>

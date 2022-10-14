12afdd2165eec805e0c13bdeee9328864acd409c
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-14 09:35:37 -0400

    Update slide_3_forgot_password.php

--- slide_3_forgot_password.php
+++ slide_3_forgot_password.php
@@ -1,34 +1,53 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Home Page
-    </title>
+<head>
     <link href='https://fonts.googleapis.com/css?family=Graduate' rel='stylesheet'>
-    <link href='slide_3.css' rel = 'stylesheet'>
-<style>
-body {
-    font-family: 'Graduate';font-size: 22px;
-    background-image: url('https://github.com/atholtonawol/raidertime-22/blob/main/assets/image/bw%20atholton%20building%20img.jpg?raw=true');
-    background-size: cover;
-}
-</style>
-  </head> 
-    <body>
-    <?php
-      require('menu_bar.html');
-    ?>
-    <div class="forgotpassword">
-     <p>Forgot Password?</p>
-      <div class="forgotpassword2">
-        <p>Enter the email address associated with your ahsraidertime account, and we will send the verification code to change your password.</p>
+    <style>
+
+        body {
+            font-family: 'Graduate';
+            background-image: url('https://github.com/atholtonawol/raidertime-22/blob/main/assets/image/bw%20atholton%20building%20img.jpg?raw=true');
+            background-size: cover;
+        }
+    </style>
+   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+  <div class="row justify-content-center text-center">
+    <div class="welcomebox col-md-6 col-10 mt-5 justify-content-center text-center">
+        <h1 class="title"> FORGOT PASSWORD?</h1>
+        <p class="words">Enter the email address associated with your ahsraidertime account, and we will send you a verification code to change your password. </p>
+     <div>
+        <input type="text" id="emailform"  name="email" placeholder="Email">
       </div>
-       <form action="" method="get">
-          <input type="text" placeholder="email"/>
-        </form>
-      <a href="slide_4_verification.php"><button class="users" type="button">Submit</button></a>
+        
+          <div class="d-flex justify-content-center">
+              <button href="slide_4_verification.php">Send Verification Code</button>
+          </div>  
+      
+    </div>
+  </div>
+  
+  
+  <footer>
+   <div class="container-fluid">
+    <div class="row">
+     <div class="col-sm-3">
+       <h5> © AWOL Team 2022,</h5>
+       </div>
+         <div class="col-sm-7">
+            <a href="slide_8_about_us.php" target="_blank">ABOUT US</a>
+           </div>
+           <div class="col-sm-2">
+             <a id="contact" href="https://www.wikipedia.org/" target="_blank">CONTACT US</a>
+       </div>
+      </div>
      </div>
+  </footer>
 
-     
-  </body>
-
+</body>
 </html>

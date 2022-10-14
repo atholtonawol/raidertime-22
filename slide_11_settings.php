0354660555ab0f06b9f8182613cc6b3f11fa29d4
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-14 07:41:59 -0400

    Update slide_11_settings.php

--- slide_11_settings.php
+++ slide_11_settings.php
@@ -5,7 +5,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="slide_11.css">
     <link href='https://fonts.googleapis.com/css?family=Graduate' rel='stylesheet'>
-    
+    <?php 
+        if (!$conn) {
+           die("Connection failed: " . mysqli_connect_error());
+         } 
+        else{
+          echo"connected";
+          }
+    ?>
     <style>
       body {
         font-family: 'Graduate';font-size: 22px;

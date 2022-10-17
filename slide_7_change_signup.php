47fffa9df2fd14ba7121d1b24c87ff268dfdd0f8
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-17 09:50:38 -0400

    Update slide_7_change_signup.php

--- slide_7_change_signup.php
+++ slide_7_change_signup.php
@@ -206,6 +206,14 @@
       </button>
       </div>
     </form>
+    <?php
+    if(isset($_POST["teacher-dropdown"]))
+    {
+    $var = $_POST["teacher-dropdown"];
+    $sql = "UPDATE studentInfo SET newTeacher = '$_POST["teacher-dropdown"]' WHERE lastName = 'Aballo'";
+    $result = mysqli_query($conn, $sql);
+    }
+    ?>
     <!--When new teacher is selected from dropdown, pass preview should show.-->
 
     <div class="pass-preview" style="background: white">
@@ -239,14 +247,7 @@
 
   
   
-  <?php
-//     if(isset($_POST["teacher-dropdown"]))
-//     {
-//       $var = $_POST["teacher-dropdown"];
-//     $sql = "UPDATE studentInfo SET newTeacher = '$_POST["teacher-dropdown"]' WHERE lastName = 'Aballo'";
-//     $result = mysqli_query($conn, $sql);
-//     }
-  ?>
+
   <!-- JavaScript -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js">
     

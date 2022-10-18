ff02431c89197daefab2bfeec58e63dda25e8d70
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-18 12:43:34 -0400

    Update backend_serverlib.php

--- backend_serverlib.php
+++ backend_serverlib.php
@@ -38,14 +38,16 @@ $column - Which column you want from the database (only one column!)
 function SER_get_teacher(int $id, String $column) {
     $sql = 'SELECT '.$column.' FROM teacher_info WHERE id='.$id;  //The query sent to the mySQL server
     $result = mysqli_query($conn, $sql);  //Connects to the SQL server and sends the $sql query. The server responds with an object containing the result of the query.
+    /*
     $var = mysqli_fetch_all($result, MYSQLI_ASSOC);  //Takes the result object from the server, and turns it into an associative array that is easy to reference in our code.
-
+    
     if (count($var) != 1) { //Ensure that exactly one result was found
         echo 'Invalid SQL result in get_teacher('.$id.'): Number of results is '.count($var).' instead of 1!';
     } else {
         //These variables will be referenced by slide_6_home.php
         return var[0][$column];
     }
+    */
 }
 
 

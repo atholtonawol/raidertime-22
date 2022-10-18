0a0b0e477d60d3cf9e09edc95870d2c1063fb270
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-18 09:41:11 -0400

    Update slide_3_forgot_password.php

--- slide_3_forgot_password.php
+++ slide_3_forgot_password.php
@@ -3,7 +3,7 @@
     <head>
     <title>Home Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="slide_2.css">
+    <link rel="stylesheet" href="slide_3.css">
     <link href='https://fonts.googleapis.com/css?family=Graduate' rel='stylesheet'>
     
 <style>

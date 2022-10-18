d7c84ad39e998efbd347c1a42af02ac94dc318ae
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-18 08:56:48 -0400

    Update slide_6_backend_get.php

--- slide_6_backend_get.php
+++ slide_6_backend_get.php
@@ -13,28 +13,6 @@ if (!$conn) {
 } 
 
 
-/*
-   $sql = 'SELECT firstName, lastName FROM student_info WHERE user="kdamav7949"';
-   $result = mysqli_query($conn, $sql);
-   $var = mysqli_fetch_all($result, MYSQLI_ASSOC);
-   echo "<br>var: ";
-   print_r($var);
-   echo "<br>sql: ";
-   print_r($sql);
-   echo "<br>result: ";
-   print_r($result);
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  exit();
-}
- echo $var[0]['firstName'];
-   foreach($var as $name)
-   {
-     echo $name['firstName'];
-     break;
-      
-   }
-*/
 
 //$userName = $_POST["username"];
 $userName = "ashahe8615";
@@ -44,7 +22,7 @@ $userName = "ashahe8615";
 if(true)
   {
     //Connect to student_info
-    $s_sql = 'SELECT firstName, lastName, hrTeacher, newTeacher FROM student_info WHERE user="'.$userName.'"';   //The query sent to the SQL server (mySQL?)
+    $s_sql = 'SELECT firstName, lastName FROM student_info WHERE user="'.$userName.'"';   //The query sent to the SQL server (mySQL?)
     $s_result = mysqli_query($conn, $sql);   //Connects to the SQL server and sends the $s_sql query. The server responds with an object containing the result of the query.
     $s_var = mysqli_fetch_all($result, MYSQLI_ASSOC);   //Takes the result object from the server, and turns it into an associative array that is easy to reference in our code.
  
@@ -55,8 +33,8 @@ if(true)
       //These variables will be referenced by slide_6_home.php
       $first_name = $s_var[0]['firstName'];
       $last_name = $s_var[0]['lastName'];
-      $hr_teacher = $s_var[0]['hrTeacher'];
-      $new_teacher = $s_var[0]['newTeacher'];
+      //$hr_teacher = $s_var[0]['hrTeacher'];
+      //$new_teacher = $s_var[0]['newTeacher'];
      /*
       //Connect to teacher_info
       $t_sql = 'SELECT name, room FROM teacher_info WHERE id='.$hr_teacher;   //The query sent to the SQL server (mySQL?)

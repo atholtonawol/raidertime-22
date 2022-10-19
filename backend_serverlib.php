4a0a1b1adaae9f6a523ed32901a69e45e6e1029e
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-19 10:01:46 -0400

    Update backend_serverlib.php

--- backend_serverlib.php
+++ backend_serverlib.php
@@ -53,14 +53,15 @@ function SER_get_teacher(int $id, String $column) {
     }
     */
     print_r($id);
-    $hr_sql = 'SELECT name FROM teacher_info WHERE id='.$id;
-        $hr_result = mysqli_query($conn, $hr_sql);
-        $hr_var = mysqli_fetch_all($hr_result, MYSQLI_ASSOC);
-        if (count($hr_var) != 1) { //Ensure that exactly one result was found
-            echo 'Invalid SQL result for teacher_info in lib: Number of results is '.count($hr_var).' instead of 1!';
+    $new_sql = 'SELECT name, room FROM teacher_info WHERE id='.$id;
+        $new_result = mysqli_query($conn, $new_sql);
+        $new_var = mysqli_fetch_all($new_result, MYSQLI_ASSOC);
+
+        if (count($new_var) != 1) { //Ensure that exactly one result was found
+            echo 'Invalid SQL result for teacher_info in LIB: Number of results is '.count($new_var).' instead of 1!';
         } else {
             //These variables will be referenced by slide_6_home.php
-            return $hr_var[0]['name'];
+            return $new_var[0]['name'];
         }
 }
 

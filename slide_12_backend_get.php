2c9f6c111f2ec4acf1e9c08c4312862dd0e73799
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-19 10:26:09 -0400

    Create slide_12_backend_get.php

---
+++ slide_12_backend_get.php
@@ -0,0 +1,49 @@
+<?php
+echo("SLIDE 12 GET<br>");
+
+require '/home4/ahsraid1/public_html/database/connect.php';
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} 
+
+
+
+//$userName = $_POST["username"];
+$userName = "ashahe8615";
+//$userName = "kdamav7949";
+//$userName = "aaball6969";
+
+$teacher_id = 75;
+
+$arr = array("hello", "world", "yes");
+  
+//if(isset($_POST['username']))
+if(true) {
+    //==== Connect to student_info ====/
+    $sql = 'SELECT fullName FROM `student_info` WHERE newTeacher='.$teacher_id;
+    $result = mysqli_query($conn, $sql);
+    $var = mysqli_fetch_all($result, MYSQLI_ASSOC); 
+    
+ 
+    if (count($var) != 1) { //Ensure that exactly one result was found
+        echo 'Invalid SQL result for student_info: Number of results is '.count($var).' instead of 1!';
+    } else {
+        //These variables will be referenced by slide_12_teacher_home.php
+        foreach($var as $i) {
+          print_r($i['fullName']);
+          }
+        
+        
+        
+        
+    }
+
+    
+} else {
+    echo "isset post username error";
+}
+
+
+
+?>

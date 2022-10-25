cc5526ede94f9a4a731d1fc3863a60ed063db715
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-25 09:24:58 -0400

    Update slide_2_log_in.php

--- slide_2_log_in.php
+++ slide_2_log_in.php
@@ -5,6 +5,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="slide_2.css">
     <link href='https://fonts.googleapis.com/css?family=Graduate' rel='stylesheet'>
+    <link href='googleSingIn.php' rel='stylesheet'>
     
 <style>
   body {

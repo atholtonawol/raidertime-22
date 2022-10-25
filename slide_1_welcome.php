12b95e1576ab405158472c72742352ec086ba7fd
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-25 10:06:39 -0400

    Update slide_1_welcome.php

--- slide_1_welcome.php
+++ slide_1_welcome.php
@@ -9,47 +9,49 @@
 <style>
   body {
     font-family: 'Graduate';font-size: 22px;
-    background-image: url('https://github.com/atholtonawol/raidertime-22/blob/main/assets/image/SchoolPicEdited.jpg?raw=true');
+    background-image: url('https://github.com/atholtonawol/raidertime-22/blob/main/assets/image/atholton_building_updated_color_crop2.jpg?raw=true');
     background-size: cover;
     background-repeat: no-repeat;
     background-color: gainsboro;
   }
-
 </style>
   </head> 
-<body>
+    <body>
     
       <div class=" px-4 pt-3 container-fluid">
       <div class="row">
-        <div class="col-10">
-          <h1 class="display-2"><b>Atholton High School</b></h1>
-            <h2 class="display-4"><b>Raider Time Signup</b></h2>
+        <div class="col-9">
+          <h1 class="display-1"><strong>Atholton High School</strong></h1>
+            <h2 class="display-2"><strong>Raider Time Signup</strong></h2>
         </div>
-        <div class="col-2 text-end">
-         <a href="https://ahs.hcpss.org/">
-           <img class = "img-fluid" src="https://github.com/atholtonawol/raidertime-22/blob/main/assets/image/NewAtholtonA.png?raw=true">
+        <div class="col-3 text-end">
+          <a href="https://ahs.hcpss.org/">
+            <img class = "img-fluid" src="https://github.com/atholtonawol/raidertime-22/blob/main/assets/image/new_Atholton_raider1-removebg-preview.png?raw=true">
          </a>
         </div>
       </div>
       </div>
     <br/>
-         
-       <?php
+      
+      <?php
      require('nav_bar.html');
    ?>
-    
+      
     <div class="container-fluid">
       <div class="row justify-content-center text-center">
-      <div class="welcomebox col-md-6 col-10 mt-5 justify-content-center text-center">    
+      <div class="welcomebox col-5 mt-5 justify-content-center text-center">    
         <p>Welcome Raiders
           <br>I am a:</p>
   
         <a href="slide_2_log_in.php"><button class="btn col-9" type="button">Student</button></a>
         <button class="btn mt-3 col-9" type="button">Teacher</button>
         <button class="btn mt-3 mb-5 col-9" type="button">Administrator</button>
-        
+      
       </div>
       </div>
+      <div class="needHelp col-2">
+        Need Help? <u>FAQs</u>
+      </div> 
     </div>
        
   </body>

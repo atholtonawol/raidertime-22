bbf94bc52babcd9cc7c8f5414cadc88bcf54ff5a
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-26 10:25:22 -0400

    Update google_sign_in_VOGUE.php

--- google_sign_in_VOGUE.php
+++ google_sign_in_VOGUE.php
@@ -1,2 +1,70 @@
+<?php
+require_once 'vendor/autoload.php';
+  
+// init configuration
+$clientID = '<YOUR_CLIENT_ID>';
+$clientSecret = '<YOUR_CLIENT_SECRET>';
+$redirectUri = '<REDIRECT_URI>';
+   
+// create Client Request to access Google API
+$client = new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectUri);
+$client->addScope("email");
+$client->addScope("profile");
+  
+// authenticate code from Google OAuth Flow
+if (isset($_GET['code'])) {
+  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+  $client->setAccessToken($token['access_token']);
+   
+  // get profile info
+  $google_oauth = new Google_Service_Oauth2($client);
+  $google_account_info = $google_oauth->userinfo->get();
+  $email =  $google_account_info->email;
+  $name =  $google_account_info->name;
+  
+  // now you can use this profile info to create account in your website and make user logged in.
+} else {
+  echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
+}
+?>
 
+	
+require_once 'vendor/autoload.php';
+
+// init configuration
+$clientID = '<YOUR_CLIENT_ID>';
+$clientSecret = '<YOUR_CLIENT_SECRET>';
+$redirectUri = '<REDIRECT_URI>';
+  
+// create Client Request to access Google API
+$client = new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectUri);
+  
+$client->addScope("email");
+$client->addScope("profile");\
+  
+// authenticate code from Google OAuth Flow
+if (isset($_GET['code'])) {
+  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+  $client->setAccessToken($token['access_token']);
+   
+  // get profile info
+  $google_oauth = new Google_Service_Oauth2($client);
+  $google_account_info = $google_oauth->userinfo->get();
+  $email =  $google_account_info->email;
+  $name =  $google_account_info->name;
+  
+  // now you can use this profile info to create account in your website and make user logged in.
+} else {
+  echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
+}
+  
+  
+  
+ 
 

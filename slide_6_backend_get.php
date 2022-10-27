4918b18fe6bc38978e1ce7d83ef04114a87485cf
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-27 09:58:12 -0400

    Update slide_6_backend_get.php

--- slide_6_backend_get.php
+++ slide_6_backend_get.php
@@ -74,19 +74,19 @@ if(true) {
         $list_var = mysqli_fetch_all($list_result, MYSQLI_ASSOC);
         
         $list_teacher = array(); //2D array: [name, room]
-        /*
+        
         if (count($list_result) <= 0) { //Ensure that results were found
             echo 'Invalid SQL result for teacher_info (list_teacher): Number of results is '.count($list_result).' instead of 1!';
         } else {
+            /*
             echo $list_result;
-            
             while($row = mysqli_fetch_array($list_result)){ //Iterates through table
                 //FRONT END FIGURE OUT FORMATTING. ALIGN THE ROOM NUMBER TO THE RIGHT
                 //echo "<option>". $row['teachers'].$row['room']."</option>";
                 $list_teacher[] = array($row['name'], $row['room']); //Appends student name to the array, PHP syntax is weird
-            }
+            }*/
             
-        }*/
+        }
 
         
     } //if one result was found

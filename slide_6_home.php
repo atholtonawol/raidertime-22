8107511dc69b600a83461cd3538eaff3bb78fb62
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-27 10:06:16 -0400

    Update slide_6_home.php

--- slide_6_home.php
+++ slide_6_home.php
@@ -69,11 +69,11 @@
         <option value="Chaudhry, Mabrooka">Chaudhry, Mabrooka</option>
         <option value="Peddicord, Scott">Peddicord, Scott</option>
          <?php 
-              echo '<option>twatafsdaedf '.count($list_teacher).'</option>';
-              echo "<option>". $list_teacher[0][0].$list_teacher[0][1]."</option>"
-             //for($i = 0; $i <= count($list_teacher); $i++) {
-             //    echo '<option>'.$list_teacher[$i][0].', '.$list_teacher[$i][1].'</option>';
-             //}
+             // echo '<option>twatafsdaedf '.count($list_teacher).'</option>';
+             // echo "<option>". $list_teacher[0][0].$list_teacher[0][1]."</option>"
+             for($i = 0; $i <= count($list_teacher); $i++) {
+                 echo '<option>'.$list_teacher[$i][0].', '.$list_teacher[$i][1].'</option>';
+             }
             /*
             require '/home4/ahsraid1/public_html/database/connect.php';
             if (!$conn) {

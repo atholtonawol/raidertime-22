e3de6c16b661d39c79b4966ab4954289eb43c1ab
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-28 09:50:30 -0400

    Update google_sign_in_VOGUE.php

--- google_sign_in_VOGUE.php
+++ google_sign_in_VOGUE.php
@@ -2,8 +2,8 @@
 require_once 'vendor/autoload.php';
   
 // init configuration
-$clientID = '751418040846-4tsmmv9vtuqcgb2k2ecc9vcsb51nm68i.apps.googleusercontent.com';
-$clientSecret = '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';
+$clientID = '751418040846-b0h7hb2us3svgl4037fjqbtmtkl9djrs.apps.googleusercontent.com';
+$clientSecret = '4aZpmW5rLXbtl5EZxZhe6Bcy';
 $redirectUri = 'https://ahsraidertime.org';
    
 // create Client Request to access Google API

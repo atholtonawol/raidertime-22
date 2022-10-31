a27909b77e07073b11ea50b170482e1e41d896c4
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-10-31 09:46:03 -0400

    Update google_sign_in_VOGUE.php

--- google_sign_in_VOGUE.php
+++ google_sign_in_VOGUE.php
@@ -14,21 +14,21 @@ $client->setRedirectUri($redirectUri);
 $client->addScope("email");
 $client->addScope("profile");
   
-// authenticate code from Google OAuth Flow
-if (isset($_GET['code'])) {
-  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-  $client->setAccessToken($token['access_token']);
+// // authenticate code from Google OAuth Flow
+// if (isset($_GET['code'])) {
+//   $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+//   $client->setAccessToken($token['access_token']);
    
-  // get profile info
-  $google_oauth = new Google_Service_Oauth2($client);
-  $google_account_info = $google_oauth->userinfo->get();
-  $email =  $google_account_info->email;
-  $name =  $google_account_info->name;
+//   // get profile info
+//   $google_oauth = new Google_Service_Oauth2($client);
+//   $google_account_info = $google_oauth->userinfo->get();
+//   $email =  $google_account_info->email;
+//   $name =  $google_account_info->name;
   
-  // now you can use this profile info to create account in your website and make user logged in.
-} else {
-  echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
-}
+//   // now you can use this profile info to create account in your website and make user logged in.
+// } else {
+//   echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
+// }
 ?>
  
 

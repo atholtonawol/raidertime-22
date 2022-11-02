d285a232e7219be401835068928a2ce514651303
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-11-02 07:49:56 -0400

    Update slide_11_settings.php

--- slide_11_settings.php
+++ slide_11_settings.php
@@ -69,14 +69,14 @@
               <option value="Stuppy, Thomas">Stuppy, Thomas</option>
               <option value="Chaudhry, Mabrooka">Chaudhry, Mabrooka</option>
               <option value="Peddicord, Scott">Peddicord, Scott</option>
-             <?php 
+                 
 //               $sql = 'SELECT teachers, room FROM teacherInfo';
 //               $result = mysqli_query($conn, $sql);
 //               while($row = mysqli_fetch_array($result)){
 //                 //FRONT END FIGURE OUT FORMATTING. ALIGN THE ROOM NUMBER TO THE RIGHT
 //               echo "<option>". $row['teachers'].$row['room']."</option>";
 //              }
-              ?>
+              
             </select>
           </td>
       
@@ -114,7 +114,7 @@
         <option value="Stuppy, Thomas">Stuppy, Thomas</option>
         <option value="Chaudhry, Mabrooka">Chaudhry, Mabrooka</option>
         <option value="Peddicord, Scott">Peddicord, Scott</option>
-         <?php 
+         
              
         
 //         if (!$conn) {
@@ -127,7 +127,7 @@
 //              //FRONT END FIGURE OUT FORMATTING. ALIGN THE ROOM NUMBER TO THE RIGHT
 //              echo "<option>". $row['teachers'].$row['room']."</option>";
 //           }
-          ?>
+          
       </select>
     </td>
       <div class="col text-center">
@@ -168,7 +168,7 @@
   </div>  
 
   
-    <?php
+    
 //     if(isset($_POST["teacher-dropdown"]))
 //     {
 //     $var = $_POST["teacher-dropdown"];
@@ -180,7 +180,7 @@
 //       }
 //     $result = mysqli_query($conn, $sql);
 //       }
-    ?>
+    
 
   <!-- JavaScript -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js">
@@ -206,8 +206,8 @@
       </div>
     </div>
       
-   <?php
-     include('footer.html');
-   ?>
+   
+//      include('footer.html');
+   
   </body>
   </html>

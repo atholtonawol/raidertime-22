08a5aef05edea193a9431022e1acc9eeadfbc110
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-11-02 09:09:08 -0400

    Update slide_12_backend_edit.php

--- slide_12_backend_edit.php
+++ slide_12_backend_edit.php
@@ -1,7 +1,16 @@
 <?php
+    require '/home4/ahsraid1/public_html/database/connect.php';
+
+    /*
     echo "<script>alert(\"backend edit\")</script>";
     if(isset($_POST['remove-0])) {
         echo "<script>alert(\"remove 0\")</script>";
     }
+    */
+
+if(isset($_POST['1_k'])){
+    $sql = 'UPDATE student_info SET newTeacher = 2 WHERE email = "habbah4377@inst.hcpss.org"';
+    $result = mysqli_query($conn, $sql);
+} 
 
 ?>

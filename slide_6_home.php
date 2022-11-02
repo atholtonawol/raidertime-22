adbcba4226476089eee2835e890ee59ecfbd6b45
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-11-02 08:55:50 -0400

    Update slide_6_home.php

--- slide_6_home.php
+++ slide_6_home.php
@@ -10,12 +10,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  <style>
-    body {
-      font-family: 'Graduate';font-size: 22px;
-      background-color: #eeeeee;
-    }
-  </style>
+    <link rel="stylesheet" href="style.css" />
+
 </head>
   
 <body>

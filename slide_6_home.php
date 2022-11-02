d2f2c2ca3cf27c7adc8349737603e6f248da8b6e
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-11-02 09:06:44 -0400

    Update slide_6_home.php

--- slide_6_home.php
+++ slide_6_home.php
@@ -1,7 +1,7 @@
 <html>
   <head>
     <!--link rel="stylesheet" href="frontEnd/test.css"-->
-
+    <link rel="stylesheet" href="slide_6.css">
     <title>Raider Time Home</title>
     <link href='https://fonts.googleapis.com/css?family=Graduate' rel='stylesheet'>
     <meta charset="utf-8">
@@ -10,9 +10,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-
-    <link rel="stylesheet" href="style.css">
-
+  <style>
+    body {
+      font-family: 'Graduate';font-size: 22px;
+      background-color: #eeeeee;
+    }
+  </style>
 </head>
   
 <body>

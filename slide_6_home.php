e6cca268152de03e5af65f20f6601c25389f23c9
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-11-02 09:08:58 -0400

    Update slide_6_home.php

--- slide_6_home.php
+++ slide_6_home.php
@@ -1,15 +1,16 @@
 <html>
   <head>
-    <!--link rel="stylesheet" href="frontEnd/test.css"-->
-    <link rel="stylesheet" href="slide_6.css">
-    <title>Raider Time Home</title>
-    <link href='https://fonts.googleapis.com/css?family=Graduate' rel='stylesheet'>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- style sheets, bootstrap, javascript -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- style sheets, bootstrap, javascript -->
+
+<link href='https://fonts.googleapis.com/css?family=Graduate' rel='stylesheet'>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+ <link rel="stylesheet" href="slide_6.css">
+    <link rel="stylesheet" href="style.css">
   <style>
     body {
       font-family: 'Graduate';font-size: 22px;

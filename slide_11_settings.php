b802cfe70b4c884a8b9853f4f2b7926a48bacb1c
atholtonawol <40551764+atholtonawol@users.noreply.github.com>
2022-11-04 09:44:07 -0400

    Update slide_11_settings.php

--- slide_11_settings.php
+++ slide_11_settings.php
@@ -7,6 +7,12 @@
     <link rel="stylesheet" href="slide_11.css">
     <link href='https://fonts.googleapis.com/css?family=Graduate' rel='stylesheet'>
 
+    <?php 
+     require '/home4/ahsraid1/public_html/database/connect.php';
+     if (!$conn) {
+       die("Connection failed: " . mysqli_connect_error());
+     } 
+    ?>
 
     
     <style>

41e9fe527b021da2c8be0b1f6ce01d97806387f4
aidanshaheen <115003641+aidanshaheen@users.noreply.github.com>
2022-11-04 09:29:11 -0400

    Update main.php

--- main.php
+++ main.php
@@ -5,12 +5,6 @@
   </header>
   <body>
     <?php
-      //include('calc.php');
-      //test
-      //header("Cache-Control: no-cache, must-revalidate");
-      //header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-      //header("Content-Type: application/xml; charset=utf-8");
-
       require('slide_1_welcome.php');
     ?>
     
